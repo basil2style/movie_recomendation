089c9a7f74a9ec1d9e368bf9d9d1ccbc084f1685
Basil <basiltalias92@gmail.com>
2018-01-28 20:03:38 -0500

    latest changes

--- movie_rec.py
+++ movie_rec.py
@@ -1,7 +1,27 @@
 
-import surprise from Reader, Dataset
+from surprise import Reader, Dataset, SVD, evaluate
 
 # Define the format
 reader = Reader(line_format='user item rating timestamp', sep='\t')
 
-# Load data from the 
+#Download dataset from http://files.grouplens.org/datasets/movielens/ml-100k.zip
+# Load data from the dataset
+data = Dataset.load_from_file('./ml-100k/u.data', reader=reader);
+
+#Split data into 5 folds
+data.split(n_folds=5)
+
+#We are using SVD
+algo = SVD()
+evaluate(algo, data, measures=['RMSE','MAE'])
+
+#retrive the trainset
+trainset = data.build_full_trainset()
+algo.fit(trainset)
+
+#Actual predication
+userid = str(196)
+itemid = str(302)
+actual_rating = 4
+
+print(algo.predict(userid, 302, 4))
